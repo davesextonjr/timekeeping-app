9cc6ee638467e626a1a95b507cfde7c75f56572e
David Sexton Jr <davesextonjr@gmail.com>
2023-11-11 22:27:48 -0500

    set up express

---
+++ timekeeping-app-backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+
+
+
+app.listen(port, () => {
+    console.log(`The Server is running on port ${port}`);
+});
